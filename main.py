ddf2e176301ea2d4ce2b2d1e4a322256a97efafe
Sergey Bilibin <s.bilibin2017@yandex.ru>
2023-09-24 09:53:09 +0300

    Initial commit.

---
+++ main.py
@@ -0,0 +1,21 @@
+import json
+import os
+
+d = {
+    "key1": {
+        "keu": 101010,
+        "key": {"key": "value"},
+        "key": {"key": "value"},
+        "key": {"key": "value"},
+        "key": {"key": "value"},
+    },
+    "keykey2": 2,
+    "key3": 10,
+    "key10": 10,
+    "sadfdsafdsafsafsadfdsafdsafsadfsafasdafdsf": 19,
+}
+
+
+class User:
+    def get_user_by_id(self, id: int):
+        ...
